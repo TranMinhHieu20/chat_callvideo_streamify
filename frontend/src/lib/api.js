434936f206a88a3cdf78ab4b07abad5e769e6725
tranminhhieu <tranhieu200304@gmail.com>
2025-12-13 10:12:34 +0700

    implemented onboarding & login

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -4,3 +4,18 @@ export const signup = async (signupData) => {
   const res = await axiosInstance.post('/auth/signup', signupData)
   return res.data
 }
+
+export const login = async (loginData) => {
+  const res = await axiosInstance.post('/auth/login', loginData)
+  return res.data
+}
+
+export const getAuthUser = async () => {
+  const res = await axiosInstance.get('auth/me')
+  return res.data
+}
+
+export const onboard = async (onboardData) => {
+  const res = await axiosInstance.post('/auth/onboarding', onboardData)
+  return res.data
+}
